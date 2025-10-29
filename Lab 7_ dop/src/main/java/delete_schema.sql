0685c557b656c3e7e783a244b1394f195854ce1b
chiecladoncoi <140918935+nghiaphamhb@users.noreply.github.com>
2025-10-30 02:15:19 +0300

    feat: Add dynamic truncate schema SQL script    Co-authored-by: TheMoonNight <241014140+dostoevski-16@users.noreply.github.com>

--- Lab 7_ dop/src/main/java/delete_schema.sql
+++ Lab 7_ dop/src/main/java/delete_schema.sql
@@ -1,1 +1,16 @@
 
+DO $$
+DECLARE
+  sql text;
+BEGIN
+  SELECT 'TRUNCATE TABLE '
+         || string_agg(format('%I.%I', schemaname, tablename), ', ')
+         || ' RESTART IDENTITY CASCADE'
+  INTO sql
+  FROM pg_tables
+  WHERE schemaname NOT IN ('pg_catalog', 'information_schema');
+
+  IF sql IS NOT NULL THEN
+    EXECUTE sql;
+  END IF;
+END$$;
